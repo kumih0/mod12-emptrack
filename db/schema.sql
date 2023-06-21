2ff4abc56b3f1bb1f8c035f66dc40fe7e4fd569a
KT <132851569+kumih0@users.noreply.github.com>
2023-06-21 12:29:59 -0500

    writing out schema sql

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS emp_db;
+CREATE DATABASE emp_db;
+
+USE emp_db;
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES departments(id)
+);
