0c67afee560683122e3a1a7aa30824d130033929
KT <132851569+kumih0@users.noreply.github.com>
2023-06-21 12:31:04 -0500

    schema tables sql

--- db/schema.sql
+++ db/schema.sql
@@ -22,3 +22,9 @@ CREATE TABLE roles (
     PRIMARY KEY (id),
     FOREIGN KEY (department_id) REFERENCES departments(id)
 );
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
