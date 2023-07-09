e5f00bedfffd531ee32a920fa01ddb31dc55c1f5
KT <132851569+kumih0@users.noreply.github.com>
2023-07-09 18:33:18 -0500

    copied addemp to server

--- server.js
+++ server.js
@@ -161,81 +161,46 @@ const addRole = () => {
 
 
 //add employee
-const addEmployee = () => {
-    db.query('SELECT id, title FROM roles', function (err, results) {
-        console.log(results);
-        let roles = [];
-        if (err) throw err;
-        roles = results.map((role) => (
-            {
-                value: role.id,
-                name: role.title,
-            })
-        );
-        console.log(roles);
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'first_name',
-                message: 'What is the first name of the employee?',
-            },
-            {
-                type: 'input',
-                name: 'last_name',
-                message: 'What is the last name of the employee?',
-            },
-            {
-                type: 'list',
-                name: 'role_id',
-                message: 'What is the role of the employee?',
-                choices: roles,
-            },
-        ])
-            .then((answers) => {
-                console.log(answers);
-                const { first_name, last_name, role_id } = answers;
-                // if (answers) { //if answers are true
-                //     db.query('INSERT INTO employees SET ?', answers, function (err, results) { if (err) throw err; console.log(`Added ${answers} to employees`);});
-                db.query('SELECT id, first_name, last_name FROM employees WHERE manager_id IS NULL', function (err, results) {
-                    console.log(results);
-                    let managerList = [];
-                    if (err) throw err;
-                    managerList = results.map((employee) => (
-                        {
-                            value: employee.id,
-                            name: employee.first_name + ' ' + employee.last_name,
-                        })
-                    );
-                    console.log(managerList);
-                    inquirer.prompt([
-                        {
-                            type: 'list',
-                            name: 'manager_id',
-                            message: 'Who is the manager of the employee?',
-                            choices: managerList,
-                        }
-                    ])
-                        .then((answers) => {
-                            console.log(answers);
-                            if (answers) {
-                                db.query(`INSERT INTO employees SET ?`, answers, function (err, results) {
-                                    console.log(results);
-                                    if (err) throw err;
-                                    console.log(`Added ${answers} to employees`);
-                                    selector();
-                                });
-                            };
-                        });
-                });
+const addEmployee = async () => {
+    const roles = await db.query('SELECT id, title FROM roles');
+    const managerList = await db.query('SELECT id, first_name, last_name FROM employees WHERE manager_id IS NULL');
 
-            });
-    });
+    const newEmployee = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'What is the first name of the employee?',
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'What is the last name of the employee?',
+        },
+        {
+            type: 'list',
+            name: 'role_id',
+            message: 'What is the role of the employee?',
+            choices: roles.map((role) => ({ value: role.id, name: role.title })),
+        },
+        {
+            type: 'list',
+            name: 'manager_id',
+            message: 'Who is the manager of the employee?',
+            choices: managerList.map((employee) => ({ value: employee.id, name: employee.first_name + ' ' + employee.last_name })),
+        }
+    ]);
+    if (newEmployee) {
+        db.query(`INSERT INTO employees SET ?`, newEmployee, function (err, results) {
+            console.log(results);
+            if (err) throw err;
+            console.log(`Added ${newEmployee} to employees`);
+            selector();
+        });
+    } else {
+        console.log('Please enter all information');
+        selector();
+    }
 };
-
-//         });
-//     });
-// };
-
 
 //remove dept
 const removeDepartment = () => {
