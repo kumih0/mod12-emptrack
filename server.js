f133e24c493c9448332d64b08cf033f6c400847f
KT <132851569+kumih0@users.noreply.github.com>
2023-07-10 12:08:04 -0500

    deleted that sandbox too soon

--- server.js
+++ server.js
@@ -229,13 +229,15 @@ const removeDepartment = () => {
             choices: departments
         }])
             .then((answers) => {
-                const dept = departments.find((department) => department.id === answers.id);
-                console.log(dept);
-                db.query('DELETE FROM departments WHERE id = ?', answers.id, function (err, results) {
-                    if (err) throw err;
-                    console.log(`Removed ${dept} from departments`);
+                console.log(answers.id - 1);
+                
+                console.log(departments[answers.id - 1]);//comes back as undefined
+                
+                // db.query('DELETE FROM departments WHERE id = ?', answers.id, function (err, results) {
+                //     if (err) throw err;
+                //     console.log(`Removed ${dept} from departments`);
                     selector();
-                });
+                // });
             });
     });
 };
